48d34449339440a6eab0acbf69528b7bdb0f1847
Charles <sircharlesaze@gmail.com>
2020-02-25 15:35:45 +0100

    Added ft_*min/max

--- src/math/ft_fmax.c
+++ src/math/ft_fmax.c
@@ -1,22 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_math.h                                       :+:      :+:    :+:   */
+/*   ft_fmax.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cacharle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/24 11:25:28 by cacharle          #+#    #+#             */
-/*   Updated: 2020/02/25 14:46:04 by cacharle         ###   ########.fr       */
+/*   Created: 2020/02/25 14:44:43 by cacharle          #+#    #+#             */
+/*   Updated: 2020/02/25 14:45:06 by cacharle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_MATH_H
-# define LIBFT_MATH_H
-
-int		ft_abs(int n);
-int		ft_max(int a, int b);
-int		ft_min(int a, int b);
-double	ft_fmax(double a, double b);
-double	ft_fmin(double a, double b);
-
-#endif
+double	ft_fmax(double a, double b)
+{
+	return (a > b ? a : b);
+}
--- src/math/ft_min.c
+++ src/math/ft_min.c
@@ -1,22 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_math.h                                       :+:      :+:    :+:   */
+/*   ft_min.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cacharle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/24 11:25:28 by cacharle          #+#    #+#             */
-/*   Updated: 2020/02/25 14:46:04 by cacharle         ###   ########.fr       */
+/*   Created: 2020/02/25 14:44:29 by cacharle          #+#    #+#             */
+/*   Updated: 2020/02/25 14:46:26 by cacharle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_MATH_H
-# define LIBFT_MATH_H
-
-int		ft_abs(int n);
-int		ft_max(int a, int b);
-int		ft_min(int a, int b);
-double	ft_fmax(double a, double b);
-double	ft_fmin(double a, double b);
-
-#endif
+int	ft_min(int a, int b)
+{
+	return (a < b ? a : b);
+}
