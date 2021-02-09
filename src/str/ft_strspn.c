fa3a34d9a84c13d4d21e6ba9a03cbbb13f375899
Charles Cabergs <me@cacharle.xyz>
2021-02-09 21:28:00 +0100

    Fixing ft_strspn return type

--- src/str/ft_strspn.c
+++ src/str/ft_strspn.c
@@ -6,7 +6,7 @@
 /*   By: cacharle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 04:29:13 by cacharle          #+#    #+#             */
-/*   Updated: 2020/02/10 04:33:11 by cacharle         ###   ########.fr       */
+/*   Updated: 2020/08/27 09:56:04 by charles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 size_t	ft_strspn(const char *s, const char *charset)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
 	while (ft_strchr(charset, s[i]) != NULL)
